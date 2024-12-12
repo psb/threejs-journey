fa31db453835af1e78ab2b78b6c71ca663e247a7
Paul Bacchus <paulbacchus@M35-117.local>
2024-12-12 15:11:10 +0000

    sliced model.

---
+++ 39-particles-cursor-animation-shader/src/shaders/particles/vertex.glsl
@@ -0,0 +1,43 @@
+uniform vec2 uResolution;
+uniform sampler2D uPictureTexture;
+uniform sampler2D uDisplacementTexture;
+
+attribute float aIntensity;
+attribute float aAngle;
+
+varying vec3 vColor;
+
+void main() {
+    // Displacment
+    vec3 newPosition=position;
+    float displacmentIntensity = texture(uDisplacementTexture, uv).r;
+    displacmentIntensity = smoothstep(0.1, 0.3, displacmentIntensity);
+
+    vec3 displacement = vec3(
+        cos(aAngle) * 0.2,
+        sin(aAngle) * 0.2,
+        1.0
+    );
+    displacement = normalize(displacement);
+    displacement *= displacmentIntensity;
+    displacement *= 3.0;
+    displacement *= aIntensity;
+
+    newPosition += displacement;
+
+    // Final position
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+
+    // Picture
+    float pictureIntensity = texture(uPictureTexture, uv).r;
+
+    // Point size
+    gl_PointSize = 0.15 * pictureIntensity * uResolution.y;
+    gl_PointSize *= (1.0 / - viewPosition.z);
+
+    // Varyings
+    vColor = vec3(pow(pictureIntensity, 2.0));
+}
